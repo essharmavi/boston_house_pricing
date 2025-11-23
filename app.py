b511da2b93e1bb96c49039dcb4e9bbe736b14e02
Vishal Sharma <vishal.sharma5181@gmail.com>
2025-11-23 23:44:02 +0530

    Adding app.py

--- app.py
+++ app.py
@@ -21,5 +21,12 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    output = model.predict(final_input)
+    return render_template("home.html", prediction_text=f"The predicted house price is {output[0]}")
+
 if __name__ == "__main__":
     app.run(debug=True)
